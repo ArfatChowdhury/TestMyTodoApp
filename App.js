d297274b7d1cb9308f8d40ce108f25b2fe76d3ae
Arfat Chowdhury <arfatahsan60@gmail.com>
2025-10-31 18:32:00 +0600

    todo app styling

--- App.js
+++ App.js
@@ -1,8 +1,44 @@
 import { Ionicons } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [searchQuery, setSearchQuery] = useState('')
+  const [addQuery, setAddQuery] = useState('')
+
+  const [data, setData] = useState([])
+
+  // console.log(searchQuery, addQuery);
+  console.log('data in array', data);
+  
+
+  const handleAdd = () => {
+
+    if(addQuery.trim() === ''){
+      Alert.alert('write task to add')
+      return
+    }
+  
+    const newTask ={
+      id: Date.now(),
+      task: addQuery
+    }
+
+    setData([...data, newTask])
+    setAddQuery('')
+  }
+
+  const handleDelete = (id) =>{
+
+    const updatedData = data.filter(item => item.id !== id)
+
+    setData(updatedData)
+
+  }
+
+
   return (
     <View style={styles.container}>
 
@@ -15,11 +51,28 @@ export default function App() {
         <TextInput
           placeholder='Search your task'
           style={styles.searchInput}
+          value={searchQuery}
+          onChangeText={setSearchQuery}
         />
       </View>
       {/* flatlist */}
 
       <View style={styles.mainContainer}>
+
+        <FlatList
+        data={data}
+        keyExtractor={item => item.id}
+        renderItem={({item})=>(
+          <View style={styles.listContainer}>
+            <Text style={styles.listTitle}>{item.task}</Text>
+            <TouchableOpacity onPress={()=>handleDelete(item.id)}>
+              <Ionicons name='trash' size={24} color='red'/>
+            </TouchableOpacity>
+          </View>
+        )}
+        />
+
+
 
       </View>
 
@@ -31,8 +84,10 @@ export default function App() {
         <TextInput
           placeholder='Add your task'
           style={styles.addInput}
+          value={addQuery}
+          onChangeText={setAddQuery}
         />
-        <TouchableOpacity>
+        <TouchableOpacity onPress={handleAdd}>
           <Ionicons name='add-circle' size={60} color={'black'} />
         </TouchableOpacity>
       </View>
@@ -58,14 +113,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 20,
     // width:'90%',
-    padding: 12,
+    padding: 10,
     marginVertical: 10,
     // marginHorizontal:10
   },
   titleText: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginVertical: 6
 
   },
   searchInput: {
@@ -74,7 +130,7 @@ const styles = StyleSheet.create({
   addInput: {
     borderWidth: 1,
     borderRadius: 20,
-    padding: 20,
+    padding: 18,
     fontSize: 16,
     width: '85%'
   },
@@ -82,9 +138,21 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: 'space-between',
     alignItems: "center",
-    marginBottom:'13%'
+    // marginBottom:'13%'
   },
   mainContainer: {
     flex: 1
+  },
+  listContainer:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    backgroundColor:'white',
+    padding:10,
+    marginVertical:10,
+    borderRadius:10
+  },
+  listTitle:{
+    fontSize:20,
+    fontWeight:'bold'
   }
 });
